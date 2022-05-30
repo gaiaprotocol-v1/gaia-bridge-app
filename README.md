33045f0222bc7c53b0c954581adf74548c4ccc9a
dilrong <dilrong@dilrong.com>
2022-05-30 17:16:23 +0900

    init

---
+++ README.md
@@ -0,0 +1,12 @@
+# Gaia Bridge App
+
+Gaia Bridge Integration App.
+
+## Get Started
+
+```
+npm install
+npm run webpack-dev
+cd docs
+swserver
+```
---
+++ src/main.ts
@@ -0,0 +1,19 @@
+import { BrowserInfo, msg } from "skydapp-browser";
+import { SkyRouter } from "skydapp-common";
+import superagent from "superagent";
+import Wallet from "./klaytn/Wallet";
+
+
+(async () => {
+    msg.language = BrowserInfo.language;
+    msg.parseCSV((await superagent.get("/msg.csv")).text);
+
+    if (sessionStorage.__spa_path) {
+        SkyRouter.go(sessionStorage.__spa_path);
+        sessionStorage.removeItem("__spa_path");
+    }
+
+    if (await Wallet.connected() !== true) {
+        await Wallet.connect();
+    }
+})();
